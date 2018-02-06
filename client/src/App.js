8aa36982a2ddc843a462cc1951f3f581297b7867
seventh1976 <seventh1976@gmail.com>
2018-02-06 18:33:59 +0800

    adding list of channels

--- client/src/App.js
+++ client/src/App.js
@@ -2,17 +2,21 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const ChannelsList = () =>
+  (<ul>
+    <li>Channel 1</li>
+    <li>Channel 2</li>
+  </ul>);
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
+        <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+          <h2>Welcome to Apollo</h2>
+        </div>
+        <ChannelsList />
       </div>
     );
   }
